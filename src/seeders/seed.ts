5d0876c87f35fa05f5041e40a9eccdf71115b986
Munir <abdullahimunir3@gmail.com>
2025-09-17 04:54:39 +0100

    fix: team_membership relation not found in db

--- src/seeders/seed.ts
+++ src/seeders/seed.ts
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import pkg from "pg";
-import * as fs from "fs";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const { Pool } = pkg;
-const databaseUrl = process.env.DB_URL;
-=======
 import { Pool } from 'pg';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -24,20 +13,10 @@ const {
 
 const databaseUrl = `postgresql://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`;
 
->>>>>>> main
 const pool = new Pool({
   connectionString: databaseUrl,
 });
 
-<<<<<<< HEAD
-if (process.env.NODE_ENV === "development") {
-  let seedQuery = fs.readFileSync(__dirname + "/exercises.seed.sql", {
-    encoding: "utf8",
-  });
-  pool.query(seedQuery, (err: Error, res: any) => {
-    console.log(err, res);
-    console.log("Seeding Completed!");
-=======
 // Use process.cwd() instead of __dirname since ts-node handles CJS differently
 const rootDir = process.cwd();
 
@@ -51,7 +30,6 @@ if (process.env.NODE_ENV === 'development') {
     } else {
       console.log('Seeding Completed!');
     }
->>>>>>> main
     pool.end();
   });
 }
